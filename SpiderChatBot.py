2b2fbbb9e12d639c984c6639c0f0b182de28190f
Manu Rajput <111367681+ManavRajput@users.noreply.github.com>
2023-02-02 03:20:10 +0530

    Update SpiderChatBot.py

--- SpiderChatBot.py
+++ SpiderChatBot.py
@@ -1,26 +1,25 @@
-# import nltk
-# import datetime
-# from nltk.stem import PorterStemmer
-# from nltk.stem import WordNetLemmatizer
-# from nltk.corpus import stopwords
-# import re
+import nltk
+import datetime
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
 import pyttsx3
-# from urllib.request import urlopen
-# import pyttsx3
-# import pywhatkit
-# import requests #pip install pyttsx3
-# import speech_recognition as sr #pip install speechRecognition
-# import datetime
-# import urllib3
-# import wikipedia #pip install wikipedia
-# import webbrowser
-# import json
-# import os
-# import smtplib
-# import pywhatkit
+from urllib.request import urlopen
+import pywhatkit
+import requests #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import urllib3
+import wikipedia #pip install wikipedia
+import webbrowser
+import json
+import os
+import smtplib
+import pywhatkit
 import pyjokes
-
-
+import pyaudio
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -31,22 +30,20 @@ def sendEmail(to, content):
     server.close()
 
 
-# def wishMe():
-#     hour = int(datetime.datetime.now().hour)
-#     if hour >= 0 and hour < 12:
-#         speak("Good Morning!")
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning!")
 
-#     elif hour >= 12 and hour < 18:
-#         speak("Good Afternoon!")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
 
-#     else:
-#         speak("Good Evening!")
+    else:
+        speak("Good Evening!")
 
-#     speak("I am spyder Sir. Please tell me how may I help you")
+    speak("I am spyder Sir. Please tell me how may I help you")
 
 
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import speech_recognition as p
 
 engine = pyttsx3.init()
 
@@ -56,13 +53,13 @@ def speak(audio):
     engine.runAndWait()
 
 
-recognizer = p.Recognizer()
+recognizer = sr.Recognizer()
 speak("Hello, Iam Spider an chatbot here to help you with your emotions.")
 speak("So how was your day today??")
 
 sum = 0
 while sum == 0:      
-    with p.Microphone() as source:
+    with sr.Microphone() as source:
         print("clearing background noise ....")
         recognizer.adjust_for_ambient_noise(source, duration=1)
         print('Waiting for your message....')
@@ -77,34 +74,34 @@ while sum == 0:
         except Exception as ex:
             text = "OPPs"
             sum = 1
-            speak("Did,t Catch that")
+            speak("Didn't Catch that")
         if  recordedaudio == 'end':
             sum = 1
         break
  
 
-# paragraph = text
+paragraph = text
 
-# sent = nltk.sent_tokenize(paragraph)
+sent = nltk.sent_tokenize(paragraph)
 
-# word = nltk.word_tokenize(paragraph)
-# lematizer = WordNetLemmatizer()
+word = nltk.word_tokenize(paragraph)
+lematizer = WordNetLemmatizer()
 
-# stemmer = PorterStemmer()
+stemmer = PorterStemmer()
 
-# # data cleaning
-# corpus = []
-# for i in range(len(sent)):
-#     review = re.sub('[^a-zA-Z]', ' ', sent[i])
-#     review = review.lower()
-#     review = review.split()
-#     review = [lematizer.lemmatize(word) for word in review if word not in set(stopwords.words('english'))]
-#     sent[i] = ' '.join(review)
-#     corpus.append(review)
+# data cleaning
+corpus = []
+for i in range(len(sent)):
+    review = re.sub('[^a-zA-Z]', ' ', sent[i])
+    review = review.lower()
+    review = review.split()
+    review = [lematizer.lemmatize(word) for word in review if word not in set(stopwords.words('english'))]
+    sent[i] = ' '.join(review)
+    corpus.append(review)
 
-# print(corpus)
 
-sentence = [str(text)]
+
+sentence = [str(corpus)]
 analyser = SentimentIntensityAnalyzer()
 for i in sentence:
     v = analyser.polarity_scores(i)
@@ -113,5 +110,6 @@ for i in sentence:
     print(v)
 if new_value == 'neg':
     speak('You seem to be sad today')
-else:
+    speak(pyjokes.get_joke())
+else: 
     speak(pyjokes.get_joke())
